0bbe9872a03789432a0de6b62226a0e9c35030bf
Karan <Karan.Chand@ru.nl>
2023-04-13 19:27:13 +0200

    Working whisper model

--- audio2text.py
+++ audio2text.py
@@ -4,11 +4,11 @@ import torch
 
 torch.cuda.empty_cache()
 
-language = 'english'
+language = 'spanish'
 
 # load model and processor
 processor = WhisperProcessor.from_pretrained("openai/whisper-medium")
-forced_decoder_ids = processor.get_decoder_prompt_ids(language="english", task="transcribe")
+forced_decoder_ids = processor.get_decoder_prompt_ids(language=language, task="transcribe")
 model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-medium")
 model.config.forced_decoder_ids = None
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
@@ -21,11 +21,10 @@ emo = emo.cast_column("audio", Audio(sampling_rate=16000))
 
 def prepare_dataset(x):
   input_features = processor(x['audio']["array"], return_tensors="pt", sampling_rate=x['audio']["sampling_rate"]).to(device).input_features
-  predicted_ids = model.generate(input_features)
-  x['model_transcription'] = processor.batch_decode(predicted_ids, skip_special_tokens=True)
+  predicted_ids = model.generate(input_features, forced_decoder_ids=forced_decoder_ids)
+  x['model_transcription'] = processor.batch_decode(predicted_ids, skip_special_tokens=True)[0]
   return x
 
-# choose how to save
-filename = "test"
+# save
 atcosim = emo.map(prepare_dataset, remove_columns='audio')
-atcosim.to_csv("output/"+filename+".csv", index = False, header=True, sep =';')
+atcosim.to_csv("output/Whisper_"+language+".csv", index = False, header=True, sep =';')
