a48590f93024097b84db2c0a3fb7fedd87001019
Karan <Karan.Chand@ru.nl>
2023-04-18 23:54:29 +0200

    Added sentiment model that works for all languages

--- text2sentiment.py
+++ text2sentiment.py
@@ -3,15 +3,23 @@ import pandas as pd
 import numpy as np
 from pathlib import Path
 
-return_all_scores = False
-classifier = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", return_all_scores=return_all_scores)
+top_k=1 # return all emotions if None, else 1
+emotion = False # use emotion, else use sentiment
+if emotion:
+    classifier = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", top_k=top_k)
+else:
+    classifier = pipeline("text-classification", model="cardiffnlp/twitter-xlm-roberta-base-sentiment", top_k=top_k)
 
-filename = 'test.csv'
-transcriptions = pd.read_csv("output/"+filename, sep=";")
+for language in ['english','italian','spanish']:
+    filename = 'Whisper_'+language+'.csv'
+    transcriptions = pd.read_csv("output/audio2text/"+filename, sep=";")
 
-emotion = np.empty(transcriptions.shape[0], dtype=object)
-for i, transcription in enumerate(transcriptions['model_transcription']):
-    emotion[i] = classifier(transcription)[0].get('label')
+    model_emotion = np.empty(transcriptions.shape[0], dtype=object)
+    for i, transcription in enumerate(transcriptions['model_transcription']):
+        model_emotion[i] = classifier(transcription)[0][0].get('label')
 
-transcriptions['emotion'] = emotion
-transcriptions.to_csv(Path("output/transcription_emotion.csv"), index=False, header=True, sep=";")
+    transcriptions['model_emotion'] = model_emotion
+    if emotion: 
+        transcriptions.to_csv(Path("output/text2emotion/transcription_emotion_"+language+".csv"), index=False, header=True, sep=";")
+    else:
+        transcriptions.to_csv(Path("output/text2sentiment/transcription_sentiment_"+language+".csv"), index=False, header=True, sep=";")
