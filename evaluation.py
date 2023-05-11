ee970ee23057ae3ac74007286d154959696b4d0a
KaranChand <karanchand@live.nl>
2023-05-11 17:45:46 +0200

    Working models

--- evaluation.py
+++ evaluation.py
@@ -6,83 +6,108 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 
-columns = np.array([])
-values = np.array([])
-for filename in os.listdir('output'):
-    df = pd.read_csv("output/"+filename)
-    y_pred = df['y_pred']
-    y_test = df['y_test']
+#################### confusion matrix
+import seaborn as sn
+import pandas as pd
+import matplotlib.pyplot as plt
 
-    columns = np.append(columns, filename[23:-4])
-    values = np.append()
+from sklearn.metrics import confusion_matrix
+df = pd.read_csv("output/text2emotion/transcription_emotion_english.csv", sep=";")
+y_pred = df['model_emotion']
+y_true = df['emotion']
+labels=df['emotion'].unique()
+array = confusion_matrix(y_true, y_pred, labels=labels)
 
-plt.figure(figsize=(8,5))
-plt.title("Mean Squared Error of Validation Set")
-plt.barh(columns, values)
-plt.ylabel("Regressor")
-plt.xlabel("y_test - y_pred")
-plt.savefig("comparison.pdf", format="pdf", bbox_inches="tight")
+
+df_cm = pd.DataFrame(array, labels, labels)
+# plt.figure(figsize=(10,7))
+sn.set(font_scale=1.4) # for label size
+sn.heatmap(df_cm, annot=True, annot_kws={"size": 16}) # font size
+
+plt.xlabel("True")
+plt.ylabel("Predicted")
+plt.tight_layout()
 plt.show()
 
+################
 
-# remove rows that have NaN as model_transcription for model evaluation
-files = ['transcribed_base_test', 'transcribed_robust_test', 'transcribed_hubert_test', 
-'transcribed_xlsr_ft_10_test', 'transcribed_xlsr_ft_50_test','transcribed_xlsr_ft_150_test',
-'transcribed_xlsr_ft_500_test', 'transcribed_xlsr_ft_1000_test', "transcribed_xlsr_ft_10_ARPA",
-"transcribed_xlsr_ft_50_ARPA", "transcribed_xlsr_ft_150_ARPA", "transcribed_xlsr_ft_5000_ARPA", 
-"transcribed_xlsr_ft_1000_ARPA", "transcribed_xlsr_ft_1000_ARPA_train", "transcribed_xlsr_ft_10_ARPA_train",
-"transcribed_xlsr_ft_50_ARPA_train", "transcribed_xlsr_ft_150_ARPA_train", "transcribed_xlsr_ft_500_ARPA_train"]
+# columns = np.array([])
+# values = np.array([])
+# for filename in os.listdir('output'):
+#     df = pd.read_csv("output/"+filename)
+#     y_pred = df['y_pred']
+#     y_test = df['y_test']
 
-files = ["transcribed_xlsr_ft_500_ARPA_train"]
-# compute indices of rows that contain NaN
-dirty_indices = set()
-for f in files:
-        df = pd.read_csv("output/" + f+".csv")
-        dirty_indices.update(df.loc[pd.isna(df["model_transcription"]), :].index.values)
+#     columns = np.append(columns, filename[23:-4])
+#     values = np.append()
 
-# evaluate dataset
-ds = load_dataset('csv', data_files='data/pruneddata.csv', split='train')
-references = ds['transcription']
+# plt.figure(figsize=(8,5))
+# plt.title("Mean Squared Error of Validation Set")
+# plt.barh(columns, values)
+# plt.ylabel("Regressor")
+# plt.xlabel("y_test - y_pred")
+# plt.savefig("comparison.pdf", format="pdf", bbox_inches="tight")
+# plt.show()
 
-# perplexity
-perplexity_metric = load_metric("perplexity", module_type="metric")
-perplexity = perplexity_metric.compute(model_id='gpt2', input_texts=references)['mean_perplexity']
 
-metrics = {"filename" : 'Atcosim',
-                   "perplexity" : perplexity
-                }
-with open('metrics/data_metrics.csv', 'w') as f:  
-    w = csv.DictWriter(f, metrics.keys())
-    w.writeheader()
-    w.writerow(metrics)
+# # remove rows that have NaN as model_transcription for model evaluation
+# files = ['transcribed_base_test', 'transcribed_robust_test', 'transcribed_hubert_test', 
+# 'transcribed_xlsr_ft_10_test', 'transcribed_xlsr_ft_50_test','transcribed_xlsr_ft_150_test',
+# 'transcribed_xlsr_ft_500_test', 'transcribed_xlsr_ft_1000_test', "transcribed_xlsr_ft_10_ARPA",
+# "transcribed_xlsr_ft_50_ARPA", "transcribed_xlsr_ft_150_ARPA", "transcribed_xlsr_ft_5000_ARPA", 
+# "transcribed_xlsr_ft_1000_ARPA", "transcribed_xlsr_ft_1000_ARPA_train", "transcribed_xlsr_ft_10_ARPA_train",
+# "transcribed_xlsr_ft_50_ARPA_train", "transcribed_xlsr_ft_150_ARPA_train", "transcribed_xlsr_ft_500_ARPA_train"]
 
-# evaluate transcriptions
-for filename in files:
-        df = pd.read_csv("output/" + filename+".csv") 
-        clean_df = df.drop(dirty_indices)
-        clean_df.to_csv(Path("data/clean_newdata.csv"), index = False, header=True)
-        print(f"removed {len(dirty_indices)} model_transcriptions that contain NaN")
+# files = ["transcribed_xlsr_ft_500_ARPA_train"]
+# # compute indices of rows that contain NaN
+# dirty_indices = set()
+# for f in files:
+#         df = pd.read_csv("output/" + f+".csv")
+#         dirty_indices.update(df.loc[pd.isna(df["model_transcription"]), :].index.values)
 
-        # load as dataset
-        ds = load_dataset('csv', data_files='data/clean_newdata.csv', split='train')
-        predictions = ds['model_transcription']
-        references = ds['transcription']
+# # evaluate dataset
+# ds = load_dataset('csv', data_files='data/pruneddata.csv', split='train')
+# references = ds['transcription']
 
-        # word error rate
-        wer_metric = load_metric("wer")
-        wer = wer_metric.compute(predictions=predictions, references=references)
+# # perplexity
+# perplexity_metric = load_metric("perplexity", module_type="metric")
+# perplexity = perplexity_metric.compute(model_id='gpt2', input_texts=references)['mean_perplexity']
 
-        # Character error rate
-        cer_metric = load_metric("cer")
-        cer = cer_metric.compute(predictions=predictions, references=references)
+# metrics = {"filename" : 'Atcosim',
+#                    "perplexity" : perplexity
+#                 }
+# with open('metrics/data_metrics.csv', 'w') as f:  
+#     w = csv.DictWriter(f, metrics.keys())
+#     w.writeheader()
+#     w.writerow(metrics)
+
+# # evaluate transcriptions
+# for filename in files:
+#         df = pd.read_csv("output/" + filename+".csv") 
+#         clean_df = df.drop(dirty_indices)
+#         clean_df.to_csv(Path("data/clean_newdata.csv"), index = False, header=True)
+#         print(f"removed {len(dirty_indices)} model_transcriptions that contain NaN")
+
+#         # load as dataset
+#         ds = load_dataset('csv', data_files='data/clean_newdata.csv', split='train')
+#         predictions = ds['model_transcription']
+#         references = ds['transcription']
+
+#         # word error rate
+#         wer_metric = load_metric("wer")
+#         wer = wer_metric.compute(predictions=predictions, references=references)
+
+#         # Character error rate
+#         cer_metric = load_metric("cer")
+#         cer = cer_metric.compute(predictions=predictions, references=references)
             
-        metrics = {"filename" : filename+ '.csv',
-                   "wer" : wer,
-                   "cer" : cer
-                }
+#         metrics = {"filename" : filename+ '.csv',
+#                    "wer" : wer,
+#                    "cer" : cer
+#                 }
 
-        df = pd.read_csv("metrics/transcribed_metrics.csv")
-        df.set_index('filename', inplace= True)
-        df.loc[filename + '.csv'] = metrics
-        df.reset_index(inplace=True)
-        df.to_csv(Path("metrics/transcribed_metrics.csv"), index = False, header=True, float_format='%.5f')
+#         df = pd.read_csv("metrics/transcribed_metrics.csv")
+#         df.set_index('filename', inplace= True)
+#         df.loc[filename + '.csv'] = metrics
+#         df.reset_index(inplace=True)
+#         df.to_csv(Path("metrics/transcribed_metrics.csv"), index = False, header=True, float_format='%.5f')
--- text2sentiment.py
+++ text2sentiment.py
@@ -4,11 +4,11 @@ import numpy as np
 from pathlib import Path
 
 top_k=1 # return all emotions if None, else 1
-emotion = False # use emotion, else use sentiment
+emotion = True # use emotion, else use sentiment
 if emotion:
-    classifier = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", top_k=top_k)
+    classifier = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", top_k=None)
 else:
-    classifier = pipeline("text-classification", model="cardiffnlp/twitter-xlm-roberta-base-sentiment", top_k=top_k)
+    classifier = pipeline("text-classification", model="cardiffnlp/twitter-xlm-roberta-base-sentiment", top_k=None)
 
 for language in ['english','italian','spanish']:
     filename = 'Whisper_'+language+'.csv'
@@ -16,10 +16,20 @@ for language in ['english','italian','spanish']:
 
     model_emotion = np.empty(transcriptions.shape[0], dtype=object)
     for i, transcription in enumerate(transcriptions['model_transcription']):
-        model_emotion[i] = classifier(transcription)[0][0].get('label')
-
+        emotion_array = classifier(transcription)[0]
+        final_emotion_dict = dict()
+        for emotion_dict in emotion_array:
+            if emotion_dict['label'] in ["anger", "joy", "fear", "disgust", "sadness"]:
+                final_emotion_dict[emotion_dict['label']] = emotion_dict['score']
+        if top_k == None:
+            model_emotion[i] = final_emotion_dict
+        else:
+            model_emotion[i] = max(final_emotion_dict, key=final_emotion_dict.get)
+    
     transcriptions['model_emotion'] = model_emotion
-    if emotion: 
+    mapping = {"joy": "happiness"}
+    transcriptions = transcriptions.replace({"model_emotion": mapping})
+    if emotion:
         transcriptions.to_csv(Path("output/text2emotion/transcription_emotion_"+language+".csv"), index=False, header=True, sep=";")
     else:
         transcriptions.to_csv(Path("output/text2sentiment/transcription_sentiment_"+language+".csv"), index=False, header=True, sep=";")
